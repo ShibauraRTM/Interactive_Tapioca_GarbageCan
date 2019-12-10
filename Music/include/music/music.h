c6e059449fe9466534279035f9adc8325e32d75a
ShibauraRTM <shibaurartm@gmail.com>
2019-12-10 16:24:03 +0900

    first commit

--- Music/include/music/music.h
+++ Music/include/music/music.h
@@ -18,13 +18,13 @@
 #include <rtm/idl/ExtendedDataTypesSkel.h>
 #include <rtm/idl/InterfaceDataTypesSkel.h>
 
- // Service implementation headers
- // <rtc-template block="service_impl_h">
+// Service implementation headers
+// <rtc-template block="service_impl_h">
 
- // </rtc-template>
+// </rtc-template>
 
- // Service Consumer stub headers
- // <rtc-template block="consumer_stub_h">
+// Service Consumer stub headers
+// <rtc-template block="consumer_stub_h">
 #include "BasicDataTypeStub.h"
 
 // </rtc-template>
@@ -50,244 +50,245 @@ using namespace RTC;
  *
  */
 class music
-	: public RTC::DataFlowComponentBase
+  : public RTC::DataFlowComponentBase
 {
-public:
-	/*!
-	 * @brief constructor
-	 * @param manager Maneger Object
-	 */
-	music(RTC::Manager* manager);
+ public:
+  /*!
+   * @brief constructor
+   * @param manager Maneger Object
+   */
+  music(RTC::Manager* manager);
 
-	/*!
-	 * @brief destructor
-	 */
-	~music();
+  /*!
+   * @brief destructor
+   */
+  ~music();
 
-	// <rtc-template block="public_attribute">
+  // <rtc-template block="public_attribute">
+  
+  // </rtc-template>
 
-	// </rtc-template>
+  // <rtc-template block="public_operation">
+  
+  // </rtc-template>
 
-	// <rtc-template block="public_operation">
+  /***
+   *
+   * The initialize action (on CREATED->ALIVE transition)
+   * formaer rtc_init_entry() 
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onInitialize();
 
-	// </rtc-template>
+  /***
+   *
+   * The finalize action (on ALIVE->END transition)
+   * formaer rtc_exiting_entry()
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onFinalize();
 
-	/***
-	 *
-	 * The initialize action (on CREATED->ALIVE transition)
-	 * formaer rtc_init_entry()
-	 *
-	 * @return RTC::ReturnCode_t
-	 *
-	 *
-	 */
-	virtual RTC::ReturnCode_t onInitialize();
+  /***
+   *
+   * The startup action when ExecutionContext startup
+   * former rtc_starting_entry()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onStartup(RTC::UniqueId ec_id);
 
-	/***
-	 *
-	 * The finalize action (on ALIVE->END transition)
-	 * formaer rtc_exiting_entry()
-	 *
-	 * @return RTC::ReturnCode_t
-	 *
-	 *
-	 */
-	 // virtual RTC::ReturnCode_t onFinalize();
+  /***
+   *
+   * The shutdown action when ExecutionContext stop
+   * former rtc_stopping_entry()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onShutdown(RTC::UniqueId ec_id);
 
-	 /***
-	  *
-	  * The startup action when ExecutionContext startup
-	  * former rtc_starting_entry()
-	  *
-	  * @param ec_id target ExecutionContext Id
-	  *
-	  * @return RTC::ReturnCode_t
-	  *
-	  *
-	  */
-	  // virtual RTC::ReturnCode_t onStartup(RTC::UniqueId ec_id);
+  /***
+   * DXライブラリをウィンドウモードにする。
+   *
+   * The activated action (Active state entry action)
+   * former rtc_active_entry()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onActivated(RTC::UniqueId ec_id);
 
-	  /***
-	   *
-	   * The shutdown action when ExecutionContext stop
-	   * former rtc_stopping_entry()
-	   *
-	   * @param ec_id target ExecutionContext Id
-	   *
-	   * @return RTC::ReturnCode_t
-	   *
-	   *
-	   */
-	   // virtual RTC::ReturnCode_t onShutdown(RTC::UniqueId ec_id);
+  /***
+   * DXライブラリを終了する。
+   *
+   * The deactivated action (Active state exit action)
+   * former rtc_active_exit()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onDeactivated(RTC::UniqueId ec_id);
 
-	   /***
-		* DXライブラリをウィンドウモードにする。
-		*
-		* The activated action (Active state entry action)
-		* former rtc_active_entry()
-		*
-		* @param ec_id target ExecutionContext Id
-		*
-		* @return RTC::ReturnCode_t
-		*
-		*
-		*/
-	virtual RTC::ReturnCode_t onActivated(RTC::UniqueId ec_id);
+  /***
+   * InPortから入ってくる数字が0から1になったときに水滴が落ちる音を
+   * 出す。
+   *
+   * The execution action that is invoked periodically
+   * former rtc_active_do()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onExecute(RTC::UniqueId ec_id);
 
-	/***
-	 *
-	 * The deactivated action (Active state exit action)
-	 * former rtc_active_exit()
-	 *
-	 * @param ec_id target ExecutionContext Id
-	 *
-	 * @return RTC::ReturnCode_t
-	 *
-	 *
-	 */
-	 // virtual RTC::ReturnCode_t onDeactivated(RTC::UniqueId ec_id);
+  /***
+   *
+   * The aborting action when main logic error occurred.
+   * former rtc_aborting_entry()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onAborting(RTC::UniqueId ec_id);
 
-	 /***
-	  * InPortから入ってくる数字が0から1になったときに水滴が落ちる音を
-	  * 出す。
-	  *
-	  * The execution action that is invoked periodically
-	  * former rtc_active_do()
-	  *
-	  * @param ec_id target ExecutionContext Id
-	  *
-	  * @return RTC::ReturnCode_t
-	  *
-	  *
-	  */
-	virtual RTC::ReturnCode_t onExecute(RTC::UniqueId ec_id);
+  /***
+   *
+   * The error action in ERROR state
+   * former rtc_error_do()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onError(RTC::UniqueId ec_id);
 
-	/***
-	 *
-	 * The aborting action when main logic error occurred.
-	 * former rtc_aborting_entry()
-	 *
-	 * @param ec_id target ExecutionContext Id
-	 *
-	 * @return RTC::ReturnCode_t
-	 *
-	 *
-	 */
-	 // virtual RTC::ReturnCode_t onAborting(RTC::UniqueId ec_id);
+  /***
+   *
+   * The reset action that is invoked resetting
+   * This is same but different the former rtc_init_entry()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onReset(RTC::UniqueId ec_id);
+  
+  /***
+   *
+   * The state update action that is invoked after onExecute() action
+   * no corresponding operation exists in OpenRTm-aist-0.2.0
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onStateUpdate(RTC::UniqueId ec_id);
 
-	 /***
-	  *
-	  * The error action in ERROR state
-	  * former rtc_error_do()
-	  *
-	  * @param ec_id target ExecutionContext Id
-	  *
-	  * @return RTC::ReturnCode_t
-	  *
-	  *
-	  */
-	  // virtual RTC::ReturnCode_t onError(RTC::UniqueId ec_id);
-
-	  /***
-	   *
-	   * The reset action that is invoked resetting
-	   * This is same but different the former rtc_init_entry()
-	   *
-	   * @param ec_id target ExecutionContext Id
-	   *
-	   * @return RTC::ReturnCode_t
-	   *
-	   *
-	   */
-	   // virtual RTC::ReturnCode_t onReset(RTC::UniqueId ec_id);
-
-	   /***
-		*
-		* The state update action that is invoked after onExecute() action
-		* no corresponding operation exists in OpenRTm-aist-0.2.0
-		*
-		* @param ec_id target ExecutionContext Id
-		*
-		* @return RTC::ReturnCode_t
-		*
-		*
-		*/
-		// virtual RTC::ReturnCode_t onStateUpdate(RTC::UniqueId ec_id);
-
-		/***
-		 *
-		 * The action that is invoked when execution context's rate is changed
-		 * no corresponding operation exists in OpenRTm-aist-0.2.0
-		 *
-		 * @param ec_id target ExecutionContext Id
-		 *
-		 * @return RTC::ReturnCode_t
-		 *
-		 *
-		 */
-		 // virtual RTC::ReturnCode_t onRateChanged(RTC::UniqueId ec_id);
-
-
-protected:
-	// <rtc-template block="protected_attribute">
-
-	// </rtc-template>
-
-	// <rtc-template block="protected_operation">
-
-	// </rtc-template>
-
-	// Configuration variable declaration
-	// <rtc-template block="config_declare">
-
-	// </rtc-template>
-
-	// DataInPort declaration
-	// <rtc-template block="inport_declare">
-	RTC::TimedLong m_1or0;
-	/*!
-	 * 0と1を受け取る
-	 */
-	InPort<RTC::TimedLong> m_1or0In;
-
-	// </rtc-template>
+  /***
+   *
+   * The action that is invoked when execution context's rate is changed
+   * no corresponding operation exists in OpenRTm-aist-0.2.0
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onRateChanged(RTC::UniqueId ec_id);
 
 
-	// DataOutPort declaration
-	// <rtc-template block="outport_declare">
+ protected:
+  // <rtc-template block="protected_attribute">
+  
+  // </rtc-template>
 
-	// </rtc-template>
+  // <rtc-template block="protected_operation">
+  
+  // </rtc-template>
 
-	// CORBA Port declaration
-	// <rtc-template block="corbaport_declare">
+  // Configuration variable declaration
+  // <rtc-template block="config_declare">
 
-	// </rtc-template>
+  // </rtc-template>
 
-	// Service declaration
-	// <rtc-template block="service_declare">
+  // DataInPort declaration
+  // <rtc-template block="inport_declare">
+  RTC::TimedLong m_1or0;
+  /*!
+   * 0と1を受け取る
+   */
+  InPort<RTC::TimedLong> m_1or0In;
+  
+  // </rtc-template>
 
-	// </rtc-template>
 
-	// Consumer declaration
-	// <rtc-template block="consumer_declare">
+  // DataOutPort declaration
+  // <rtc-template block="outport_declare">
+  
+  // </rtc-template>
 
-	// </rtc-template>
+  // CORBA Port declaration
+  // <rtc-template block="corbaport_declare">
+  
+  // </rtc-template>
 
-private:
-	// <rtc-template block="private_attribute">
-	int flag;
-	// </rtc-template>
+  // Service declaration
+  // <rtc-template block="service_declare">
+  
+  // </rtc-template>
 
-	// <rtc-template block="private_operation">
+  // Consumer declaration
+  // <rtc-template block="consumer_declare">
+  
+  // </rtc-template>
 
-	// </rtc-template>
+ private:
+  // <rtc-template block="private_attribute">
+	 int flag;
+  // </rtc-template>
+
+  // <rtc-template block="private_operation">
+  
+  // </rtc-template>
 
 };
 
 
 extern "C"
 {
-	DLL_EXPORT void musicInit(RTC::Manager* manager);
+  DLL_EXPORT void musicInit(RTC::Manager* manager);
 };
 
 #endif // MUSIC_H
